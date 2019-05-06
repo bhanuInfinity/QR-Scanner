d220744191edf51b2b83c1087980acd0e36d2294
waseeminfi <waseem.ahmad@infinitylabs.in>
2019-05-06 19:04:24 +0530

    Created all Screen with changes

--- app/src/main/java/com/example/sdwan/LoginScreen.java
+++ app/src/main/java/com/example/sdwan/LoginScreen.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.sdwan.utils.Utils;
 
 public class LoginScreen extends AppCompatActivity implements View.OnClickListener {
 
@@ -23,10 +26,19 @@ public class LoginScreen extends AppCompatActivity implements View.OnClickListen
         user_name = findViewById(R.id.user_name);
         password = findViewById(R.id.password);
         submit = findViewById(R.id.submit);
+        submit.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+         if(user_name.getText().toString().trim().length()>0 && password.getText().toString().trim().length()>0){
+             if(Utils.INTANCE.ValidateLogin(user_name.getText().toString().trim(),password.getText().toString().trim())){
 
+             }else{
+                 Toast.makeText(this,"Please enter correct login detail",Toast.LENGTH_SHORT).show();
+             }
+         }else{
+             Toast.makeText(this,"Please enter correct login detail",Toast.LENGTH_SHORT).show();
+         }
     }
 }
---
+++ app/src/main/java/com/example/sdwan/utils/Constant.java
@@ -0,0 +1,6 @@
+package com.example.sdwan.utils;
+
+public class Constant {
+    public static final String USERNAME="infinity1";
+    public static final String PASSWORD ="infinity1234";
+}
